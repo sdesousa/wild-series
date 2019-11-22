1dd2c178c382efc4de43b9b2d31aa1587f71aba4
sdesousa <sylvaindesousa@free.fr>
2019-11-22 17:00:43 +0100

    wild series 03

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index() :Response
+    {
+        return $this->render('home.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+
+}
