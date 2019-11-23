2253ec0031f58f1eadcb7a35d00fb601546cc732
sdesousa <sylvaindesousa@free.fr>
2019-11-23 20:58:11 +0100

    wild series 05 routing

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,16 +6,28 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * @Route("/wild", name="wild_")
+ */
 Class WildController extends AbstractController
 {
     /**
-     * @Route("/wild", name="wild_index")
+     * @Route("/index", name="index")
      */
-    public function index() :Response
+    public function index(): Response
     {
         return $this->render('wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
     }
 
+    /**
+     * @Route("/show/{slug<[a-z0-9.-]+>?}",
+     *     name="show")
+     */
+    public function show(?string $slug): Response
+    {
+        $slug = (!isset($slug)) ? "Aucune série sélectionnée, veuillez choisir une série" : ucwords(str_replace('-', ' ', $slug));
+        return $this->render('wild/show.html.twig', ['slug' => $slug]);
+    }
 }
