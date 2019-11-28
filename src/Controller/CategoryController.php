749d9ae4e721434f5a4758d9e60e1ec6e72add9d
sdesousa <sylvaindesousa@free.fr>
2019-11-28 11:55:57 +0100

    wild series 11 form

---
+++ src/Controller/CategoryController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Category;
+use App\Form\CategoryType;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/category", name="category_")
+ */
+class CategoryController extends AbstractController
+{
+
+    /**
+     *
+     * @Route("/index", name="index")
+     * @return Response
+     */
+    public function index(): Response
+    {
+        $categories = $this->getDoctrine()->getRepository(Category::class)->findAll();
+        if (!$categories) {
+                throw $this->createNotFoundException('No category found.');
+        }
+        return $this->render(
+            'category/index.html.twig', [
+            'categories' => $categories,
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/add", name="add")
+     * @return Response
+     */
+    public function add(EntityManagerInterface $entityManager, Request $request):Response
+    {
+        $category = new Category();
+        $form = $this->createForm(CategoryType::class, $category, ['method' => Request::METHOD_GET]);
+        $form->handleRequest($request);
+        if ($form->isSubmitted()) {
+            $data = $form->getData();
+            // $data contient les données du $_POST
+            // TODO : Faire une recherche dans la BDD avec les infos de $data...
+            $category->setName($data->getName());
+            $entityManager->persist($category);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('category_index');
+        }
+        return $this->render(
+            'category/add.html.twig', [
+            'form' => $form->createView(),
+        ]);
+
+    }
+}
--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -6,7 +6,10 @@ use App\Entity\Category;
 use App\Entity\Episode;
 use App\Entity\Program;
 use App\Entity\Season;
+use App\Form\CategoryType;
+use App\Form\ProgramSearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -26,10 +29,16 @@ Class WildController extends AbstractController
         if (!$programs) {
             throw $this->createNotFoundException('No program found in program\'s table.');
         }
-
-        return $this->render('wild/index.html.twig', [
-            'programs' => $programs,
-        ]);
+        $form = $this->createForm(
+            ProgramSearchType::class,
+            null,
+            ['method' => Request::METHOD_GET]
+        );
+        return $this->render(
+            'wild/index.html.twig', [
+                'programs' => $programs,
+                'form' => $form->createView(),
+            ]);
     }
 
     /**
