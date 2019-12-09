471eae42665cf077185d7a2cdab6439d9ecc6519
sdesousa <sylvaindesousa@free.fr>
2019-12-09 14:40:54 +0100

    wild series 17

--- src/Controller/ProgramController.php
+++ src/Controller/ProgramController.php
@@ -9,6 +9,8 @@ use App\Service\Slugify;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
@@ -16,6 +18,7 @@ use Symfony\Component\Routing\Annotation\Route;
  */
 class ProgramController extends AbstractController
 {
+
     /**
      * @Route("/", name="program_index", methods={"GET"})
      */
@@ -28,8 +31,9 @@ class ProgramController extends AbstractController
 
     /**
      * @Route("/new", name="program_new", methods={"GET","POST"})
+     * @throws \Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
-    public function new(Request $request, Slugify $slugify): Response
+    public function new(Request $request, Slugify $slugify, MailerInterface $mailer): Response
     {
         $program = new Program();
         $form = $this->createForm(ProgramType::class, $program);
@@ -40,6 +44,13 @@ class ProgramController extends AbstractController
             $program->setSlug($slugify->generate($program->getTitle()));
             $entityManager->persist($program);
             $entityManager->flush();
+
+            $email = (new Email())
+                ->from($this->getParameter('mailer_from'))
+                ->to($this->getParameter('mailer_to'))
+                ->subject('Une nouvelle série: ' . $program->getTitle() . ' vient d\'être publiée !')
+                ->html($this->renderView('program/email/notification.html.twig', ['program' => $program]));
+            $mailer->send($email);
 
             return $this->redirectToRoute('program_index');
         }
