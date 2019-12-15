f9330f2df1cbb6976f805938653209ba57b6d382
sdesousa <sylvaindesousa@free.fr>
2019-12-15 16:23:10 +0100

    wild-series Q20 inscription

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProgramRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,17 @@ Class DefaultController extends AbstractController
     /**
      * @Route("/", name="app_index")
      */
-    public function index() :Response
+    public function index(ProgramRepository $programRepository) :Response
     {
+        $programs = $programRepository->findBy(
+            [],
+            ['id' => 'DESC'],
+            3
+        );
+
         return $this->render('home.html.twig', [
             'website' => 'Wild Séries',
+            'programs' => $programs,
         ]);
     }
 
