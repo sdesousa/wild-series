2ff1995d40c217507ac82abb63499a297d00ba29
sdesousa <sylvaindesousa@free.fr>
2019-12-16 13:36:33 +0100

    wild series 19 fix access comments

--- src/Entity/Program.php
+++ src/Entity/Program.php
@@ -5,9 +5,12 @@ namespace App\Entity;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 
 /**
  * @ORM\Entity(repositoryClass="App\Repository\ProgramRepository")
+ * @UniqueEntity(fields={"title"}, message="ce titre existe déjà")
  */
 class Program
 {
